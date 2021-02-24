d2c7e418f198d00ad0138ee43a4e2e2edddafd05
hamza-twyf <hamza15maghraby@gmail.com>
2021-02-23 22:02:03 -0800

    sss

---
+++ js/js.js
@@ -0,0 +1,13 @@
+var g = document.getElementsByClassName("rob"),
+    string = "Hamza",
+    i = 0;
+
+function count() {
+    "use strict";
+    rob.textContent += string[i];
+    i++;
+
+
+    if (i > string.length) {
+        clearInterval(clear);
+        div.textContent = }
